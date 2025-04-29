67b9bc772d4676bc8ba442bbb8fc8b51c2431b65
RegmiS <sandeshreg35@gmail.com>
2025-04-29 17:38:10 -0500

    Removing tmux theme because it sucks and default is better

--- nvim/lua/plugins/mdprev.lua
+++ nvim/lua/plugins/mdprev.lua
@@ -1,5 +1,5 @@
-return {
-	"lukas-reineke/headlines.nvim",
-	dependencies = "nvim-treesitter/nvim-treesitter",
-	config = true,
-}
+-- return {
+-- 	"lukas-reineke/headlines.nvim",
+-- 	dependencies = "nvim-treesitter/nvim-treesitter",
+-- 	config = true,
+-- }
