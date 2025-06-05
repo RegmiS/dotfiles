9dbab4462375feb71038127a9862cf092f82ccad
RegmiS <sandeshreg35@gmail.com>
2025-06-04 21:44:48 -0500

    Fixing errors and updating to new version of neovim

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,53 +5,33 @@ return {
 			require("mason").setup()
 		end,
 	},
+
 	{
 		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"rust_analyzer",
-					"ts_ls",
-					"pyright",
-					"lua_ls",
-					"html",
-					"cssls",
-					"emmet_ls",
-				},
-				automatic_installation = true,
-			})
-			local lspconfig = require("lspconfig")
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					lspconfig[server_name].setup({})
-				end,
-			})
-		end,
-	},
-	-- handles not just lsp's but also tools like formatters and linters and lsps
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					-- "prettier",
-					-- "stylua",
-					-- "eslint_d",
-					-- "clangd",
-				},
-				auto_update = true, -- Automatically update tools
-				run_on_start = true, -- Install missing tools when Neovim starts
+				-- remove this entirely if you want to manually install LSPs via :Mason
+				ensure_installed = { "lua_ls", "pyright" }, -- safe servers
+				automatic_installation = false,
 			})
 		end,
 	},
 
-	-- Install nvim-lspconfig (LSP client for Neovim)
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			-- Additional LSP configurations if needed
+			-- manually setup each server you care about
+			local lspconfig = require("lspconfig")
+
+			-- Lua
+			lspconfig.lua_ls.setup({})
+
+			-- Python
+			lspconfig.pyright.setup({})
 		end,
 	},
 }
